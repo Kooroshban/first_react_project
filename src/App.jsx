ecbee9e74c9c8f61fcac5e6c2530b6d220b82bfb
Kooroshban <kooroshbanyadm@gmail.com>
2026-08-20 18:13:36 +0330

    not wpa

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,35 @@
 import { useState } from "react";
+
 const App = () => {
+
   const [count, setCount] = useState(0);
+  //batching
+  // const minFive = () => {
+  //   setCount(count - 5);
+  //   setCount(count - 5);
+  //   setCount(count - 5);
+  //   setCount(count - 5);
+  // };
+  // const minFive = () => {
+  //   setCount((prev) => prev -5);
+  //   setCount((prev) => prev -5);
+  //   setCount((prev) => prev -5);
+  // };
+
+  
+  const minFive = () => {
+    setCount(count - 5);
+  };
+
   return (
     <div className="bg-green-700 text-center ">
       <h1 className="text-3xl font-bold mb-6">React Counter</h1>
+
       <div className="rounded-xl bg-bg-200 flex flex-col items-center gap-3 p-9">
         <h2 className="font-semibold text-3xl w-162.5">{count}</h2>
         <div className="flex gap-3 flex-wrap ">
           <button
-            onClick={() => {
-              setCount(count - 5);
-            }}
+            onClick={minFive}
             className="w-32.5 h-12.5 rounded-lg border border-border"
           >
             -5
@@ -41,7 +60,7 @@ const App = () => {
           </button>
         </div>
         <button
-          onClick={()=>setCount(0)}
+          onClick={() => setCount(0)}
           className="w-32.5 h-12.5 rounded-lg border border-border"
         >
           🔄 Reset
